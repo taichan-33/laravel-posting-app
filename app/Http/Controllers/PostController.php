697cf78e73d4958d3186cb81f96ab63ef2a82903
taichan-03 <tatataiyan3@gmail.com>
2025-09-01 22:36:51 +0900

    投稿一覧、詳細画面編集

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Post;
+use App\Http\Requests\PostRequest;
+
+class PostController extends Controller
+{
+    // 一覧ページ
+    public function index()
+    {
+        $posts = Auth::user()->posts()->orderBy('created_at', 'asc')->get();
+
+        return view('posts.index', compact('posts'));
+    }
+
+    public function show(Post $post)
+    {
+        return view('posts.show', compact('post'));
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store(PostRequest $request)
+    {
+        // 投稿のタイトルのバリデーションを追加
+        $request->validate([
+            'title' => 'required|string|max:40',
+            'content' => 'required|string|max:200',
+        ]);
+        
+        $post = new Post();
+        $post->title = $request->input('title');
+        $post->content = $request->input('content');
+        $post->user_id = Auth::id();
+        $post->save();
+
+        return redirect()->route('posts.index')->with('flash_message', '投稿が完了しました。');
+    }
+
+    public function edit(Post $post)
+    {
+        if ($post->user_id !== Auth::id()) {
+            return redirect()->route('posts.index')->with('error_message', '不正なアクセスです。');
+        }
+
+        return view('posts.edit', compact('post'));
+    }
+
+    // 更新機能
+    public function update(PostRequest $request, Post $post)
+    {
+        if ($post->user_id !== Auth::id()) {
+            return redirect()->route('posts.index')->with('error_message', '不正なアクセスです');
+        }
+
+        $post->title = $request->input('title');
+        $post->content = $request->input('content');
+        $post->save();
+
+        return redirect()->route('posts.show', $post)->with('flash_message', '投稿を編集しました。');
+    }
+
+    // 削除機能
+    public function destroy(Post $post)
+    {
+        if ($post->user_id !== Auth::id()) {
+            return redirect()->route('posts.index')->with('error_message', '不正なアクセスです。');
+        }
+
+        $post->delete();
+
+        return redirect()->route('posts.index')->with('flash_message', '投稿を削除しました。');
+    }
+}
--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -17,9 +17,9 @@ class PostFactory extends Factory
     public function definition(): array
     {
         return [
-            'user_id' => 1,
-            'title' => fake()->sentence(),
-            'content' => fake()->realText(),
+            'user_id' => 3,
+            'title' => fake()->realText(20, 5),
+            'content' => fake()->realText(200, 5),
         ];
     }
 }
